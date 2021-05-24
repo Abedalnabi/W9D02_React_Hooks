0dd622853ed838803bccee84bc4eb06cef91358e
M_abedalnabi <abedalnabi96@gmail.com>
2021-05-24 22:55:01 +0300

    pulse 4

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import React, { useState } from "react";
 import "./App.css";
 
 const App = () => {
-  const [post, setPost] = useState(0);
+  const [post, setPost] = useState([
+    { userId: 1, id: 101, title: "Mohammad", body: "mohammad" },
+    { userId: 2, id: 102, title: "alnabale", body: "mohammad" },
+  ]);
 
   return (
     <div>
