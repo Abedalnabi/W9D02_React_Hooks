257028f8d1aba12772f31ed2087deda190bc355a
M_abedalnabi <abedalnabi96@gmail.com>
2021-05-24 22:41:47 +0300

    pulse 1+2

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import React from "react";
 import "./App.css";
 
 const App = () => {
-  return <h1>MERAKddI Academy</h1>;
+  return (
+    <div>
+      <h1>Blog App</h1>
+    </div>
+  );
 };
 
 export default App;
