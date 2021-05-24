00e1a5e69c3d9932a32c5c7fb1233736162ea29b
M_abedalnabi <abedalnabi96@gmail.com>
2021-05-24 23:07:36 +0300

    pulse 5

--- src/App.js
+++ src/App.js
@@ -3,13 +3,23 @@ import "./App.css";
 
 const App = () => {
   const [post, setPost] = useState([
-    { userId: 1, id: 101, title: "Mohammad", body: "mohammad" },
-    { userId: 2, id: 102, title: "alnabale", body: "mohammad" },
+    { userId: 1, id: 101, title: "Mohammad", body: "mohammadBody" },
+    { userId: 2, id: 102, title: "alnabale", body: "mohammadBody2" },
   ]);
+  const array = post.map((ele, i) => {
+    return (
+      <div key={i}>
+        <p> {ele.title}</p>
+        <p>{ele.body}</p>
+      </div>
+    );
+  });
+  console.log(post[0]);
 
   return (
     <div>
       <h1>Blog App</h1>
+      {array}
     </div>
   );
 };
